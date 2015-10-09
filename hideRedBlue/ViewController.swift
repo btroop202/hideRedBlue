0c7d63e952b1ee30ad642a9daea5f4e0d0e3239f
btroop202 <stop_ing@hotmail.com>
2015-10-09 14:07:58 +0200

    Inital commit

--- hideRedBlue/ViewController.swift
+++ hideRedBlue/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var blueBombImg: UIImageView!
+    @IBOutlet weak var redBombImg: UIImageView!
+    
+    @IBOutlet weak var hideBlueBombBtn: UIButton!
+    @IBOutlet weak var hideRedBombBtn: UIButton!
+    @IBOutlet weak var showAll: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+
+    
+    @IBAction func hideBlueBomb(sender: AnyObject) {
+        blueBombImg.hidden = true
+        redBombImg.hidden = false
+    }
+  
+    @IBAction func hideRedBomb(sender: AnyObject) {
+        redBombImg.hidden = true
+        blueBombImg.hidden = false
+    }
+    
+    
+    @IBAction func showAllBtn(sender: AnyObject) {
+        redBombImg.hidden = false
+        blueBombImg.hidden = false
+    }
+    
 
 }
 
